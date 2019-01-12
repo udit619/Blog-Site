d38d3f48d312be87cbe25d22c472503dc3eeecd7
Udit Namdev <unamdev0@gmail.com>
2019-01-12 18:29:10 +0530

    Added article now creating new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blogapp');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/technology',function (req,res) {
+  res.render('article',{topic:"Technology"});
+});
+
+router.get('/personal',function (req,res) {
+    res.render('article',{topic:"Personal"});
+});
+
+router.get('/sports',function (req,res) {
+    res.render('article',{topic:"Sports"});
+});
+
+router.get('/politics',function (req,res) {
+    res.render('article',{topic:"Politics"});
+});
+
+
+
 
 module.exports = router;
